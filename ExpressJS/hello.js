ec2fe218f28b2a259226432eeba0de5e494e6f43
Hung <hunghaui2310@gmail.com>
2020-04-19 22:09:30 +0700

    commit

--- ExpressJS/hello.js
+++ ExpressJS/hello.js
@@ -1,61 +1,31 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-const adapter = new FileSync('db.json');
-const db = low(adapter);
-const shortid = require('shortid');
-
-db.defaults({ users: []})
-    .write();
 const app = express();
+const bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var db = require('./db');
+var userRoute = require('./routes/user.route');
+var authRoute = require('./routes/auth.route');
+var authMiddleWare = require('./middlewares/auth.middleware');
+
 const port = 3000;
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+app.use(express.static('public'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/', (req, res) =>
     res.render('index', {
         name: 'HungNN'
     })
 );
-app.get('/users', function (req, res) {
-    res.render('users/index', {
-        users: db.get('users').value()
-    });
-});
 
-app.get('/users/search', function (req, res) {
-    var q = req.query.q;
-    var matchUsers = users.filter(function (user) {
-        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-    });
-    res.render('users/index', {
-        users: matchUsers
-    })
-});
-
-app.get('/users/create', function (req, res) {
-   res.render('users/create');
-});
-
-app.get('/users/:id', function (req, res) {
-   var id = req.params.id;
-   console.log(id);
-   var user = db.get('users').find({ id: id }).value();
-   res.render('users/view', {
-       user: user
-   })
-});
-
-app.post('/users/create', function (req, res) {
-    req.body.id = shortid.generate();
-    db.get('users').push(req.body).write();
-    res.redirect('/users');
-});
+app.use('/users', authMiddleWare.requireAuth, userRoute);
+app.use('/auth', authRoute);
 
 app.listen(3000, function () {
     console.log('Server is listening on port ' + port);
---
+++ ExpressJS/routes/user.route.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var validate = require('../validate/user.validate');
+var db = require('../db');
+var controller = require('../controllers/user.controller');
+
+router.get('/', controller.index);
+
+router.get('/cookie', function (req, res, next) {
+    res.cookie('user-id', 12345);
+    res.send('Hello');
+})
+
+router.get('/search', controller.search);
+
+router.get('/create', controller.create);
+
+router.get('/:id', controller.detail);
+
+router.post('/create', validate.postCreate, controller.postCreate);
+
+module.exports = router;
